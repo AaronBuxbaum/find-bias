864a6367ec7b6c4f4ecbc23b6d6a3da5118ae817
Aaron Buxbaum <me@aaronbuxbaum.com>
2019-07-08 10:32:54 -0400

    can use app only auth

--- server/src/twitter.ts
+++ server/src/twitter.ts
@@ -3,8 +3,7 @@ const Twit = require('twit')
 const Twitter = new Twit({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token: process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    app_only_auth: true
 });
 
 export const getTweets = async (handle: string) => {
