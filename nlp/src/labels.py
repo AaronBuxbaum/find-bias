9acb31381a5983b17ab362db452728b6d04539d6
Aaron Buxbaum <me@aaronbuxbaum.com>
2019-08-06 00:53:10 -0400

    add missing labels

--- nlp/src/labels.py
+++ nlp/src/labels.py
@@ -1,6 +1,8 @@
 from spacy import explain
 
-labels = ['CARDINAL', 'DATE', 'MONEY', 'ORDINAL', 'PERCENT', 'QUANTITY', 'TIME', 'ORG', 'PERSON', 'GPE', 'NORP', 'WORK_OF_ART', 'FAC', 'LOC', 'LAW', 'EVENT', 'PRODUCT']
+labels = ['CARDINAL', 'DATE', 'MONEY', 'ORDINAL', 'PERCENT', 'QUANTITY', 'TIME', 'ORG', 'PERSON', 'GPE', 'NORP', 'WORK_OF_ART', 'FAC', 'LANGUAGE', 'LOC', 'LAW', 'EVENT', 'PRODUCT']
+
+wikipedia_corpus_labels = ['PER', 'LOC', 'ORG', 'MISC']
 
 explanations = map(lambda x: {
     x: explain(x)
