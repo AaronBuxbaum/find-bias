7b7e50d803389685abf4e7d577d887b93ce40852
Aaron Buxbaum <me@aaronbuxbaum.com>
2019-08-21 00:24:13 -0400

    Split out orm config

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -1,25 +1,40 @@
 // tslint:disable object-literal-sort-keys
-const prefix = "src/database";
+import { trimEnd } from "lodash";
 
-export = {
+const database = {
   type: "postgres",
   port: process.env.POSTGRES_SERVICE_PORT,
-  host: process.env.POSTGRES_SERVICE_HOST,
+  host: process.env.POSTGRES_SERVICE_HOST
+};
 
+const authentication = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  database: process.env.POSTGRES_DB
+};
 
+const options = {
   synchronize: true,
-  logging: false,
+  logging: false
+};
 
-  entities: [`${prefix}/entity/**/*.ts`],
-  migrations: [`${prefix}/migration/**/*.ts`],
-  subscribers: [`${prefix}/subscriber/**/*.ts`],
+const prefix = "src/database";
+const cli = {
+  entitiesDir: `${prefix}/entity`,
+  migrationsDir: `${prefix}/migration`,
+  subscribersDir: `${prefix}/subscriber`
+};
 
-  cli: {
-    entitiesDir: `${prefix}/entity`,
-    migrationsDir: `${prefix}/migration`,
-    subscribersDir: `${prefix}/subscriber`
-  }
+const stripDir = (str: string) => trimEnd(str, "Dir");
+const folders: IDictionary<string[]> = {};
+Object.entries(cli).forEach(([key, value]) => {
+  folders[stripDir(key)] = [`${value}/**/*.ts`];
+});
+
+export = {
+  cli,
+  ...authentication,
+  ...database,
+  ...folders,
+  ...options
 };
